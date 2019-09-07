16bdc2d689074e89c1b811898cfa9ead15efb035
Andy Wendt <andy@awendt.com>
2019-09-06 22:46:33 -0600

    Convert over to mechanize

--- Gemfile
+++ Gemfile
@@ -2,6 +2,8 @@
 source "https://rubygems.org"
 
 gem "nokogiri"
+gem "mechanize"
+gem "pry"
 
 group :test do
   gem 'rspec'
--- src/run.rb
+++ src/run.rb
@@ -1,11 +1,19 @@
 require 'open-uri'
 require 'openssl'
-require 'nokogiri'
+require 'mechanize'
 require 'json'
+require 'pry'
 
 url = 'https://farepay.rideuta.com'
-html = open(url, {ssl_verify_mode: OpenSSL::SSL::VERIFY_NONE})
 
-doc = Nokogiri::HTML(html)
-login = doc.css('.login')
+store = OpenSSL::X509::Store.new
+store.add_file File.dirname(__FILE__) + '/digicert-root-ca.crt'
+
+agent = Mechanize.new
+agent.cert_store = store
+agent.verify_mode = OpenSSL::SSL::VERIFY_NONE
+page = agent.get(url)
+
+login = page.links.find { |link| link.text.include? 'Log In'}
+
 pp login.text
