ee9dbb8dfec6d160d5e03322d795687e616aa7a9
Andy Wendt <andy@awendt.com>
2019-09-07 13:18:11 -0600

    Wait for ajax

--- src/run.rb
+++ src/run.rb
@@ -1,5 +1,6 @@
 require 'kimurai'
 require 'yaml'
+require 'pry'
 
 class UtaSpider < Kimurai::Base
   @name = "UTA Spider"
@@ -8,6 +9,7 @@ class UtaSpider < Kimurai::Base
 
   def parse(response, url:, data: {})
     credentials = YAML.load_file(File.expand_path('~') + '/.uta/secret.yml')
+    values = []
     browser.fill_in "j_username", with: credentials['username']
     browser.fill_in "j_password", with: credentials['password']
     browser.click_button "Login"
@@ -19,8 +21,20 @@ class UtaSpider < Kimurai::Base
     browser.find('//*[@id="cardSeletor"]/option[2]').click
     browser.find('//*[@id="dateRangeSeletor"]/option[2]').click
     response = browser.current_response
+    wait_for_ajax
+    binding.pry
     tr = browser.all('//*[@id="data"]/tbody/tr/td[4]')
     tr.each { |td| pp td.text }
+  end
+
+  def wait_for_ajax
+    Timeout.timeout(Capybara.default_max_wait_time) do
+      loop until finished_all_ajax_requests?
+    end
+  end
+
+  def finished_all_ajax_requests?
+    browser.evaluate_script('jQuery.active').zero?
   end
 end
 
